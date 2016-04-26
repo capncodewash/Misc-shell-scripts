1c306e52034f4c4a9ef190f31018be0ed5bb2848
Aleksej <deletesoftware@yandex.ru>
2016-04-26 12:56:53 +0300

    List both files only in dest and only in source.
    Also prepend "cmp_" to all names of list files.

--- find_missing_files.sh
+++ find_missing_files.sh
@@ -32,17 +32,18 @@ echo "Dest dir: $dest_dir">>cmp_dirs.txt
 
 # Files into which to write checksums of all files.
 # These will be cleared before output begins.
-source_files="source_files_unsorted.txt"
-dest_files="dest_files_unsorted.txt"
+source_files="cmp_source_files_unsorted.txt"
+dest_files="cmp_dest_files_unsorted.txt"
 
 # for sorted checksum lists - these are used to make the comparisons
 
-SOURCE="source_files_sorted.txt"
-DEST="dest_files_sorted.txt"
+SOURCE="cmp_source_files_sorted.txt"
+DEST="cmp_dest_files_sorted.txt"
 
 # The file which should contain the final output.
 # This will be cleared before output begins.
-output_file="missing_files.txt"
+output_file1="cmp_files_only_in_source.txt"
+output_file2="cmp_files_only_in_dest.txt"
 
 
 # right, let's get on with it.
@@ -51,5 +52,8 @@ find $source_dir -type f -exec sha512sum {} > $source_files \;
 find $dest_dir -type f -exec sha512sum {} > $dest_files \;
 sort $source_files > $SOURCE
 sort $dest_files > $DEST
-for sha1 in `comm -23 <(cat $SOURCE | awk '{print $1}') <(cat $DEST | awk '{print $1}')`; do grep $sha1 $SOURCE; done | awk '{print $2}' > $output_file
-echo "Finished finding missing files. Output written to $output_file."
+
+for sha1 in `comm -23 <(cat $SOURCE | awk '{print $1}') <(cat $DEST | awk '{print $1}')`; do grep $sha1 $SOURCE; done | awk '{print $2}'|sort|uniq> $output_file1
+for sha1 in `comm -13 <(cat $SOURCE | awk '{print $1}') <(cat $DEST | awk '{print $1}')`; do grep $sha1 $DEST; done | awk '{print $2}'|sort|uniq> $output_file2
+
+echo "Finished finding missing files. Output written to $output_file1 and $output_file2"
